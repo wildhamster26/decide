cede72af87b9e9cd5d16cd679dc4234f836bf39e
wildhamster26 <38760849+wildhamster26@users.noreply.github.com>
2018-06-11 15:13:51 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+This is a game for my son, that's supposed to help motivate him to type, and by so doing help him learn how to read and write.
---
+++ decide.js
@@ -0,0 +1,22 @@
+let mainText = document.querySelector("#mainText"),
+	image = document.querySelector("#image"),
+	option1 = document.querySelector("#option1"),
+	option2 = document.querySelector("#option2"),
+	input = document.querySelector("#input"),
+	userInput = document.querySelector("#userInput"),
+	userName = document.querySelector("#userName");
+	userImage = document.querySelector("#userImage");
+
+userName.focus();
+
+
+
+//Tasks:
+//1. Event handlers to recieve user name and image.
+//2. Write and take pictures of scenes and create a scene DB.
+//3. Change page data according to the scene (main text, image, options 1+2).
+//4. Event handler for the user input text box. Desensitize capitulation.
+//5. Background.
+//6. Use user's image on the regular pictures to "put them in the scene".
+//7. Create a button to return to the first scene without re-entering name and image file.
+//8. Split up to seperate files to make it easier to understand.
