b10dcd36633fb563ad2128e64f80dbe373d146e7
wildhamster26 <38760849+wildhamster26@users.noreply.github.com>
2018-06-11 17:20:42 +0000

    No file, only text inputs
    Appearantly a local file upload without a DB is not as easy as I thought.

--- decide.js
+++ decide.js
@@ -1,13 +1,37 @@
+"use strict";
+
 let mainText = document.querySelector("#mainText"),
-	image = document.querySelector("#image"),
+	image = document.querySelector("#mainImage"),
 	option1 = document.querySelector("#option1"),
 	option2 = document.querySelector("#option2"),
 	input = document.querySelector("#input"),
 	userInput = document.querySelector("#userInput"),
-	userName = document.querySelector("#userName");
-	userImage = document.querySelector("#userImage");
+	userNameInput = document.querySelector("#userName");
 
-userName.focus();
+
+userNameInput.focus();
+
+let player;
+
+userNameInput.addEventListener("keypress", (event) => {
+	//Take user name and picture and put in variables.
+	if (event.keyCode === 13) {
+		player = userNameInput.value;
+		//Make the form disappear from the screen
+		userNameInput.classList.add("none");
+		//Make all scene elements appear
+		userInput.classList.remove("none");
+		image.classList.remove("none");
+	}
+});
+
+
+userInput.addEventListener("keypress", (event) => {
+	//Take user name and picture and put in variables.
+	if (event.keyCode === 13) {
+		
+	}
+});
 
 
 
