b899cad9c2f19b9ee5e60004c443015cbcd38605
wildhamster26 <38760849+wildhamster26@users.noreply.github.com>
2018-06-14 16:40:07 +0000

    Added scenes 1,2,3,5,6,8,9

--- decide.js
+++ decide.js
@@ -119,3 +119,4 @@ userInput.addEventListener("keypress", (event) => {
 //5. Background.
 //7. Create a button to return to the first scene without re-entering name and image file.
 //9. Enable name customization for all scenes with the player's name as given by the userNameInput
+//10. Center and enlarge the text for the options and main.
